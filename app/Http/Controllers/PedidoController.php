a50070f95e62efe8e8bb21b5a65a037fea28916f
Luis21 <huamanluis808@gmail.com>
2023-05-22 21:35:15 -0500

    Deployment comienzo

---
+++ app/Http/Controllers/PedidoController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PedidoCollection;
+use Carbon\Carbon;
+use App\Models\Pedido;
+use Illuminate\Http\Request;
+use App\Models\PedidoProducto;
+use Illuminate\Support\Facades\Auth;
+
+class PedidoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return new PedidoCollection(Pedido::with('user')->with('productos')->where('estado', 0)->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //almacenar una orden
+        $pedido = new Pedido;
+        $pedido->user_id = Auth::user()->id;
+        $pedido->total = $request->total;
+        $pedido->save();
+
+
+        //obtener el ID del pedido
+        $id = $pedido->id;
+
+        //Obtener los productos
+        $productos = $request->productos;
+
+        //Formatear un arreglo
+        $pedido_producto = [];
+
+        foreach ($productos as $producto) {
+            $pedido_producto[] = [
+                'pedido_id' => $id,
+                'producto_id' => $producto['id'],
+                'cantidad' => $producto['cantidad'],
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ];
+        }
+
+        //almacenar en la BD
+        PedidoProducto::insert($pedido_producto);
+
+        return [
+            'message'   => 'Pedido realizado correctamente, estara listo en unos minutos'
+        ];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pedido $pedido)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pedido $pedido)
+    {
+        $pedido->estado = 1;
+        $pedido->save();
+        return [
+            'pedodo' => $pedido
+        ];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pedido $pedido)
+    {
+        //
+    }
+}
